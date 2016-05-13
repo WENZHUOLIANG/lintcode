ca880b984ae19d9d0c575672c39a8ae027e488ab
WENZHUOLIANG <wen@Wenzhuos-MacBook-Pro.local>
2016-05-12 23:53:46 -0500

    Test for submitting solution 3

---
+++ Code/threesumclosest/Solution3.java
@@ -0,0 +1,13 @@
+package threesumclosest;
+
+public class Solution3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test for solution 3");
+
+	}
+
+}
